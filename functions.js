e01ec313bcbc6a6857d59ffb54f5385d5286d0a9
alichan <alice.hitugi@gmail.com>
2021-10-20 22:31:17 +0900

    修正

--- functions.js
+++ functions.js
@@ -18,19 +18,51 @@ exports.configureMysql = async () => {
 }
 
 // ======================
+// APIレスポンス
+// ======================
+exports.apiResponse = (code = 0, data = 0, message = '成功') => {
+    return {
+        code: code,
+        data: data,
+        message: message,
+    }
+}
+
+// ===============================
+// ポストデータのキーの存在チェック
+// ===============================
+exports.isExistKey = (requiredKeys, postData) => {
+    for (let i in requiredKeys) {
+        if (!(requiredKeys[i] in postData)) {
+            return false
+        }
+    }
+
+    return true
+}
+
+// ===========================
+// ポストデータの文字数チェック
+// ===========================
+exports.isStrOutOfRange = (value, min, max) => {
+    if (value.length < min || value.length > max) return false
+    return true
+}
+
+// ======================
 // ユーザー認証
 // ======================
-exports.authenticateUser = async (uid) => {
+exports.authenticateUser = async (userId) => {
     // データベースに接続してユーザーidを取得し、そのユーザーidが受け取ったidと同値だったらtrueを返し、それ以外はfalseを返す
     const connection = await this.configureMysql()
 
     if (!connection) return false
 
     try {
-        const sql = `SELECT * FROM users WHERE user_id = ${uid}`
+        const sql = `SELECT * FROM users WHERE user_id = ${userId}`
         const [rows] = await connection.query(sql)
 
-        if (uid === rows[0]['user_id']) {
+        if (userId === rows[0]['user_id']) {
             return true
         } else {
             return false
--- routes/register-sort.js
+++ routes/register-sort.js
@@ -4,9 +4,68 @@ const func = require('../functions')
 const router = express.Router()
 
 router.post('/', async (req, res) => {
+    const postData = req.body
+
+    // ポストされたデータの必須キーの存在チェック
+    const requiredKeys = ['user_id', 'name', 'description', 'image', 'items']
+    if (!func.isExistKey(requiredKeys, postData))
+        res.send(func.apiResponse(1, 0, 'ポストデータのキーが不足しています'))
+
+    // ポストされたデータをそれぞれ変数に格納
+    const userId = postData['user_id']
+    const name = postData['name']
+    const description = postData['description']
+    const image = postData['image']
+    const items = postData['items']
+
+    // ポストされたデータのitems中の必須キーの存在チェック
+    const requiredItemKeys = ['itemName', 'itemImage']
+    for (let i in items) {
+        if (!func.isExistKey(requiredItemKeys, items[i]))
+            res.send(
+                func.apiResponse(1, 0, 'ポストデータのキーが不足しています')
+            )
+    }
+
+    // バリデーション
+    if (!func.isStrOutOfRange(userId, 1, 128))
+        res.send(func.apiResponse(1, 0, 'ユーザーIDの文字数が範囲外です'))
+    if (!func.isStrOutOfRange(name, 1, 255))
+        res.send(func.apiResponse(1, 0, 'ソートの名前の文字数が範囲外です'))
+    if (!func.isStrOutOfRange(description, 1, 255))
+        res.send(func.apiResponse(1, 0, 'ソートの説明の文字数が範囲外です'))
+    if (!func.isStrOutOfRange(image, 1, 2083))
+        res.send(
+            func.apiResponse(1, 0, 'ソートの画像のURLの文字数が範囲外です')
+        )
+
+    for (let i in items) {
+        if (!func.isStrOutOfRange(items[i]['itemName'], 1, 255))
+            res.send(
+                func.apiResponse(
+                    1,
+                    0,
+                    'ソートアイテムの名前の文字数が範囲外です'
+                )
+            )
+
+        if (!func.isStrOutOfRange(items[i]['itemImage'], 1, 2083))
+            res.send(
+                func.apiResponse(
+                    1,
+                    0,
+                    'ソートアイテムの画像のURLの文字数が範囲外です'
+                )
+            )
+    }
+
+    if (items.length < 1 || items.length > 100)
+        res.send(func.apiResponse(1, 0, 'ソートアイテムの数が範囲外です'))
+
+    res.send('成功')
+
     // ユーザー認証を実行
-    const result = await func.authenticateUser('1')
-    res.send(result)
+    // const result = await func.authenticateUser('1')
 })
 
 module.exports = router
