c1c214a0d733ecbcda9af3fd8987a957839e391c
alichan <alice.hitugi@gmail.com>
2021-10-21 17:19:35 +0900

    データベースに画像以外のソートとソートアイテムの情報を登録できるようにした

--- routes/register-sort.js
+++ routes/register-sort.js
@@ -7,7 +7,7 @@ router.post('/', async (req, res) => {
     const postData = req.body
 
     // ポストされたデータの必須キーの存在チェック
-    const requiredKeys = ['user_id', 'name', 'description', 'image', 'items']
+    const requiredKeys = ['user_id', 'name', 'description', 'itemName']
     if (!func.isExistKey(requiredKeys, postData))
         res.send(func.apiResponse(1, 0, 'ポストデータのキーが不足しています'))
 
@@ -15,56 +15,30 @@ router.post('/', async (req, res) => {
     const userId = postData['user_id']
     const name = postData['name']
     const description = postData['description']
-    const image = postData['image']
-    const items = postData['items']
+    const itemName = postData['itemName']
 
     // その他データベースに登録する値を変数に格納
     const deleteFlg = false
     const createDate = func.formatDate(new Date())
     const updateDate = func.formatDate(new Date())
 
-    // ポストされたデータのitems中の必須キーの存在チェック
-    const requiredItemKeys = ['itemName', 'itemImage']
-    for (let i in items) {
-        if (!func.isExistKey(requiredItemKeys, items[i]))
-            res.send(
-                func.apiResponse(1, 0, 'ポストデータのキーが不足しています')
-            )
-    }
-
     // バリデーション
     if (!func.isStrOutOfRange(userId, 1, 128))
         res.send(func.apiResponse(1, 0, 'ユーザーIDの文字数が範囲外です'))
     if (!func.isStrOutOfRange(name, 1, 255))
-        res.send(func.apiResponse(1, 0, 'ソートの名前の文字数が範囲外です'))
-    if (!func.isStrOutOfRange(description, 1, 255))
-        res.send(func.apiResponse(1, 0, 'ソートの説明の文字数が範囲外です'))
-    if (!func.isStrOutOfRange(image, 1, 2083))
         res.send(
-            func.apiResponse(1, 0, 'ソートの画像のURLの文字数が範囲外です')
+            func.apiResponse(1, 0, 'ソートタイトルの名前の文字数が範囲外です')
+        )
+    if (!func.isStrOutOfRange(description, 1, 255))
+        res.send(
+            func.apiResponse(1, 0, 'ソートタイトルの説明の文字数が範囲外です')
+        )
+    if (!func.isStrOutOfRange(itemName, 1, 255))
+        res.send(
+            func.apiResponse(1, 0, 'ソートアイテムの名前の文字数が範囲外です')
         )
 
-    for (let i in items) {
-        if (!func.isStrOutOfRange(items[i]['itemName'], 1, 255))
-            res.send(
-                func.apiResponse(
-                    1,
-                    0,
-                    'ソートアイテムの名前の文字数が範囲外です'
-                )
-            )
-
-        if (!func.isStrOutOfRange(items[i]['itemImage'], 1, 2083))
-            res.send(
-                func.apiResponse(
-                    1,
-                    0,
-                    'ソートアイテムの画像のURLの文字数が範囲外です'
-                )
-            )
-    }
-
-    if (items.length < 1 || items.length > 100)
+    if (itemName.length < 1 || itemName.length > 100)
         res.send(func.apiResponse(1, 0, 'ソートアイテムの数が範囲外です'))
 
     // ユーザー認証を実行
@@ -78,14 +52,32 @@ router.post('/', async (req, res) => {
     if (!connection)
         res.send(func.apiResponse(1, 0, 'データベースに接続できませんでした'))
 
-    // ソートを登録し、登録できたら正常なレスポンスをを返し、それ以外はエラーレスポンスを返す
+    // ソートタイトルとソートアイテムを登録する
     try {
-        // ソートの登録
-        const sql = `INSERT INTO sorts (name,description,image,user_id,delete_flg,create_date,update_date) values ('${name}','${description}','${image}','${userId}',${deleteFlg},'${createDate}','${updateDate}')`
-        await connection.query(sql)
+        // ソートタイトルの登録
+        const sql = `INSERT INTO sorts (name,description,image,user_id,delete_flg,create_date,update_date) values ('${name}','${description}','','${userId}',${deleteFlg},'${createDate}','${updateDate}')`
+        const [rows] = await connection.query(sql)
+        const sortId = rows.insertId
 
-        res.send(func.apiResponse(0, 0, '成功'))
+        // ソートアイテムの登録
+        let sortItemIds = []
+
+        for (let i in itemName) {
+            let sql = `INSERT INTO sort_items (name,image,sort_id,delete_flg,create_date,update_date) values ('${itemName[i]}','','${sortId}','${deleteFlg}','${createDate}','${updateDate}')`
+            const [rows] = await connection.query(sql)
+            sortItemIds.push(rows.insertId)
+        }
+
+        // 登録できたらdataに登録したソートタイトルとソートアイテムのidを記載した正常なレスポンスをを返す
+        res.send(
+            func.apiResponse(
+                0,
+                { sort_id: sortId, sort_item_ids: sortItemIds },
+                '成功'
+            )
+        )
     } catch (e) {
+        // エラーがひっかかったらエラーレスポンスを返す
         res.send(func.apiResponse(1, 0, 'ソートを登録できませんでした'))
     } finally {
         connection.end()
