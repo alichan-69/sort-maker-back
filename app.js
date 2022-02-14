17a4c5b2d493cf923982931645c751c8ce0ebcbf
alichan <alice.hitugi@gmail.com>
2022-02-14 22:36:31 +0900

    ファイルをgitに追加

--- app.js
+++ app.js
@@ -1,12 +1,16 @@
 const express = require('express')
 const router = require('./routes')
 const cors = require('cors')
+const dotenv = require('dotenv')
 
 const port = process.env.port || 3000
 const app = express()
 
 // CORS有効化
 app.use(cors())
+
+// 環境変数有効化
+dotenv.config()
 
 // リクエストパラメータをjsonで受け取り、配列も設定できるようにする
 app.use(express.urlencoded({ extended: true }))
